688afd8f2c9fb4d354a88a68fdcedb2e534bf28b
Eric.Cui <465785032@qq.com>
2021-08-26 14:39:07 +0800

    test

---
+++ CXZSpringBootWeb/cxzspringboot_web/src/main/java/com/cxz/cxzspringboot_web/CxzspringbootWebApplication.java
@@ -0,0 +1,13 @@
+package com.cxz.cxzspringboot_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxzspringbootWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CxzspringbootWebApplication.class, args);
+    }
+
+}
