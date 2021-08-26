1853502d9f138d5f57c53d6e8adfa94c4307a75c
Eric.Cui <465785032@qq.com>
2021-08-26 16:38:53 +0800

    new

--- CXZSpringBootWeb/cxzspringboot_web/src/main/java/com/cxz/cxzspringboot_web/config/SwaggerConfig.java
+++ CXZSpringBootWeb/cxzspringboot_web/src/main/java/com/cxz/cxzspringboot_web/config/SwaggerConfig.java
@@ -1,7 +1,14 @@
 package com.cxz.cxzspringboot_web.config;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 /**
  * @author cxz
